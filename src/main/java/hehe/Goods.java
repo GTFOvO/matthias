9bbb72362da16e5969ea34a196b7f8fd0f9925e1
issuser <294942509@qq.com>
2021-02-02 17:56:43 +0800

    11

--- src/main/java/hehe/Goods.java
+++ src/main/java/hehe/Goods.java
@@ -4,5 +4,11 @@ public class Goods {
     public static void main(String[] args) {
         System.out.println("Hello!!");
         System.out.println("Hello!!");
+        System.out.println("Hello!!");
+        System.out.println("Hello!!");
+        System.out.println("Hello!!");
+        System.out.println("Hello!!");
+        System.out.println("Hello!!");
+        System.out.println("Hello!!");
     }
 }
